0de51129389320a1b58aba6bdc06a79fc059ff68
Nekraket _ <ekaterinanekrasova89798@gmail.com>
2026-02-25 22:31:57 +0700

    Фабричный метод

---
+++ lab3/WpfApp1/Creators.cs
@@ -0,0 +1,100 @@
+﻿using System.Windows.Media;
+
+//абстрактный создатель для кругов
+public abstract class CircleCreator
+{
+    public abstract Circle CreateCircle();
+}
+
+// Конкретные создатели для разных цветов
+public class RedCircleCreator : CircleCreator
+{
+    public override Circle CreateCircle()
+    {
+        return new Circle { Color = Colors.Red };
+    }
+}
+
+public class BlueCircleCreator : CircleCreator
+{
+    public override Circle CreateCircle()
+    {
+        return new Circle { Color = Colors.Blue };
+    }
+}
+
+public class GreenCircleCreator : CircleCreator
+{
+    public override Circle CreateCircle()
+    {
+        return new Circle { Color = Colors.Green };
+    }
+}
+
+
+
+
+// Абстрактный создатель для квадратов
+public abstract class SquareCreator
+{
+    public abstract Square CreateSquare();
+}
+
+// Конкретные создатели для разных цветов
+public class RedSquareCreator : SquareCreator
+{
+    public override Square CreateSquare()
+    {
+        return new Square { Color = Colors.Red };
+    }
+}
+
+public class BlueSquareCreator : SquareCreator
+{
+    public override Square CreateSquare()
+    {
+        return new Square { Color = Colors.Blue };
+    }
+}
+
+public class GreenSquareCreator : SquareCreator
+{
+    public override Square CreateSquare()
+    {
+        return new Square { Color = Colors.Green };
+    }
+}
+
+
+
+
+// Абстрактный создатель для треугольников
+public abstract class TriangleCreator
+{
+    public abstract Triangle CreateTriangle();
+}
+
+// Конкретные создатели для разных цветов
+public class RedTriangleCreator : TriangleCreator
+{
+    public override Triangle CreateTriangle()
+    {
+        return new Triangle { Color = Colors.Red };
+    }
+}
+
+public class BlueTriangleCreator : TriangleCreator
+{
+    public override Triangle CreateTriangle()
+    {
+        return new Triangle { Color = Colors.Blue };
+    }
+}
+
+public class GreenTriangleCreator : TriangleCreator
+{
+    public override Triangle CreateTriangle()
+    {
+        return new Triangle { Color = Colors.Green };
+    }
+}
--- lab3/WpfApp1/MainWindow.xaml.cs
+++ lab3/WpfApp1/MainWindow.xaml.cs
@@ -1,22 +1,64 @@
-﻿using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfApp1
 {
     public partial class MainWindow : Window
     {
-        //проверка
+        private CircleCreator _currentCircleCreator;
+        private SquareCreator _currentSquareCreator;
+        private TriangleCreator _currentTriangleCreator;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            UpdateFigures();
+        }
+
+        private void UpdateFigures()
+        {
+            if (FiguresPanel == null)
+            {
+                return;
+            }
+
+            if (ColorComboBox.SelectedItem != null)
+            {
+                ComboBoxItem selectedItem = (ComboBoxItem)ColorComboBox.SelectedItem;
+
+                switch (selectedItem.Content.ToString())
+                {
+                    case "Красный":
+                        _currentCircleCreator = new RedCircleCreator();
+                        _currentSquareCreator = new RedSquareCreator();
+                        _currentTriangleCreator = new RedTriangleCreator();
+                        break;
+                    case "Синий":
+                        _currentCircleCreator = new BlueCircleCreator();
+                        _currentSquareCreator = new BlueSquareCreator();
+                        _currentTriangleCreator = new BlueTriangleCreator();
+                        break;
+                    case "Зеленый":
+                        _currentCircleCreator = new GreenCircleCreator();
+                        _currentSquareCreator = new GreenSquareCreator();
+                        _currentTriangleCreator = new GreenTriangleCreator();
+                        break;
+                    default:
+                        return;
+                }
+
+                FiguresPanel.Children.Clear();
+
+                FiguresPanel.Children.Add(_currentCircleCreator.CreateCircle().CreateUIElement());
+                FiguresPanel.Children.Add(_currentSquareCreator.CreateSquare().CreateUIElement());
+                FiguresPanel.Children.Add(_currentTriangleCreator.CreateTriangle().CreateUIElement());
+            }
+        }
+
+        private void ColorComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateFigures();
         }
     }
 }
