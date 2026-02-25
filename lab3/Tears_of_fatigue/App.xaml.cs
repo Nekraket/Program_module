8103dc1dad04f9a843b7f3e9dd4f052b0ea60c89
Nekraket <144684137+Nekraket@users.noreply.github.com>
2026-02-25 17:45:37 +0700

    Add files via upload
    Работай родненький гитхаб

---
+++ lab3/Tears_of_fatigue/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Tears_of_fatigue
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
