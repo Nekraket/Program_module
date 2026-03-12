0da5f41b2dd0d363c46639d717f3204baa6a07bd
Nekraket _ <ekaterinanekrasova89798@gmail.com>
2026-03-12 11:18:32 +0700

    переделан main

--- lab5/ConsoleApp1/Program.cs
+++ lab5/ConsoleApp1/Program.cs
@@ -57,7 +57,6 @@
 
             IFileSystem fileSystem = new FileSystemAdapter(root);
 
-
             Console.WriteLine("Список содержимого корневой папки:");
             var rootItems = fileSystem.ListItems("/");
             foreach (var item in rootItems)
@@ -65,22 +64,18 @@
                 Console.WriteLine($"  {item}");
             }
 
-            Console.WriteLine("\nСписок содержимого папки 'Медиа':");
-            var mediaItems = fileSystem.ListItems("/Медиа");
-            foreach (var item in mediaItems)
-            {
-                Console.WriteLine($"  {item}");
-            }
-
-            Console.WriteLine("\nСписок содержимого папки 'Фото':");
-            var photoItems = fileSystem.ListItems("/Медиа/Фото");
-            foreach (var item in photoItems)
-            {
-                Console.WriteLine($"  {item}");
-            }
-
             Console.WriteLine("\nЧтение файла:");
-            fileSystem.ReadFile("/Медиа/video.mp4");
+            byte[] fileData = fileSystem.ReadFile("/Медиа/video.mp4");
+            Console.WriteLine($"Прочитано байт: {fileData.Length}");
+            if (fileData.Length > 0)
+            {
+                Console.Write("Содержимое (первые 8 байт): ");
+                for (int i = 0; i < fileData.Length; i++)
+                {
+                    Console.Write($"{fileData[i]} ");
+                }
+                Console.WriteLine();
+            }
 
             Console.WriteLine("\nЗапись файла:");
             fileSystem.WriteFile("/Документы/newfile.txt", new byte[] { 1, 2, 3, 4 });
@@ -88,8 +83,9 @@
             Console.WriteLine("\nУдаление элемента:");
             fileSystem.DeleteItem("/Рабочее/presentation.pptx");
 
-            Console.WriteLine("\nПопытка прочитать папку как файл (должна быть ошибка):");
-            fileSystem.ReadFile("/Медиа");
+            Console.WriteLine("\nПопытка прочитать папку как файл:");
+            byte[] folderData = fileSystem.ReadFile("/Медиа");
+            Console.WriteLine($"Прочитано байт: {folderData.Length}");
 
 
 
