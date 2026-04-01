c2673d323e7b3b38543cc6c250dd011fb8115850
Nekraket _ <ekaterinanekrasova89798@gmail.com>
2026-04-01 22:50:40 +0700

    Этап 2: Стратегия

---
+++ lab7/ConsoleApp1/FormatsStrategy.cs
@@ -0,0 +1,28 @@
+﻿namespace ConsoleApp1
+{
+    public interface IFormatStrategy
+    {
+        string Format(string message, DateTime timestamp);
+    }
+
+
+    public class TextFormatStrategy : IFormatStrategy
+    {
+        public string Format(string message, DateTime timestamp) => $"[{timestamp:yyyy-MM-dd HH:mm:ss}] {message}";
+    }
+
+
+    public class JsonFormatStrategy : IFormatStrategy
+    {
+        public string Format(string message, DateTime timestamp) => $"{{\"timestamp\":\"{timestamp:O}\",\"message\":\"{message}\"}}";
+    }
+
+
+    public class HtmlFormatStrategy : IFormatStrategy
+    {
+        public string Format(string message, DateTime timestamp)
+        {
+            return $"<div class='event'><span class='time'>[{timestamp:HH:mm:ss}]</span> <span class='message'>{message}</span></div>";
+        }
+    }
+}
