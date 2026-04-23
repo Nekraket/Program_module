d3976b9c4726de0851330e7e51cca1199f87dca7
Nekraket _ <ekaterinanekrasova89798@gmail.com>
2026-04-23 07:14:24 +0700

    не обращаем внимание

--- lab10/PhoneBook/Views/MainWindow.xaml.cs
+++ lab10/PhoneBook/Views/MainWindow.xaml.cs
@@ -2,10 +2,6 @@
 
 namespace PhoneBook.Views
 {
-    /// <summary>
-    /// Представление главного окна приложения.
-    /// Код в code-behind отсутствует (кроме InitializeComponent).
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
