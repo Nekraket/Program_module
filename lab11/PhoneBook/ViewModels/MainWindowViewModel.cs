91e5124c7361886bf6a829012dc35ffb655ea61e
Nekraket _ <ekaterinanekrasova89798@gmail.com>
2026-04-28 21:56:30 +0700

    Создание экрана «О программе» и оболочки Shell

---
+++ lab11/PhoneBook/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows.Input;
+using PhoneBook.Services;
+using PhoneBook.ViewModels.Base;
+
+namespace PhoneBook.ViewModels
+{
+    /// <summary>
+    /// ViewModel для главного окна (Shell).
+    /// Управляет навигацией через меню.
+    /// </summary>
+    public class MainWindowViewModel : ObservableObject
+    {
+        private readonly INavigationService _navigationService;
+
+        public MainWindowViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+
+            ShowContactsCommand = new RelayCommand(ShowContacts);
+            ShowAboutCommand = new RelayCommand(ShowAbout);
+
+            _navigationService.NavigateTo<ContactsListViewModel>();
+        }
+
+        public INavigationService NavigationService => _navigationService;
+
+        public ICommand ShowContactsCommand { get; }
+        public ICommand ShowAboutCommand { get; }
+
+        private void ShowContacts()
+        {
+            _navigationService.NavigateTo<ContactsListViewModel>();
+        }
+
+        private void ShowAbout()
+        {
+            _navigationService.NavigateTo<AboutViewModel>();
+        }
+    }
+}
